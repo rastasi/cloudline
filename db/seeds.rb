e32e9c786a22d41862d8694aafab9ea91492b4d8
Zsolt Tasnadi <tasi@syrakuza.hu>
2016-01-02 21:45:39 +0100

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-#=begin
 User.create!(
   email: 'admin@example.com',
   password: 'password',
   password_confirmation: 'password'
 )
-#=end
 
 Site.create!(
   name: 'Syrakuza',
   url: 'syrakuza.hu',
   protocol: 'http',
-  active: true
+  active: true,
+  check_interval: 10,
+  user_id: 1
 )
 
 Site.create!(
   name: 'Teletype.hu',
   url: 'teletype.hu',
   protocol: 'http',
-  active: true
+  active: true,
+  check_interval: 10,
+  user_id: 1
 )
