a29e3889c08b5e4b133313e92b54a28662b994a1
Tasnádi Zsolt <tasi@syrakuza.hu>
2016-01-03 16:32:53 +0100

    colored status, yes_no helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def formatted_status status
+    "<span class='status #{status}'>#{"status.#{status}".t}</span>"
+  end
+
+  def yes_no bool
+    bool ? 'yes'.t : 'no'.t
+  end
+  
 end
